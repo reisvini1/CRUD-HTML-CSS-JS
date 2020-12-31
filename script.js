69bf427782470148e1e6237df33be37e2e9b9a08
Vinícius Reis <vinicius.moreira2003@gmail.com>
2020-12-31 10:57:19 -0300

    adding savebtn functions(still need to be fixed)

--- script.js
+++ script.js
@@ -1,28 +1,62 @@
-var dados = []
+var dados = [1]
 
 function populaTabela() {
     if (Array.isArray(dados)) {
+
+        localStorage.setItem("__dados__", JSON.stringify(dados))
 
         $("#tblDados tbody").html("")
 
         dados.forEach(function (item) {
             //TEMPLATE STRING
             $("#tblDados tbody").append(`<tr>
-                <td>$${item.ID}</td>
-                <td>$${item.Nome}</td>
-                <td>$${item.Sobrenome}</td>
-                <td>$${item.DtNascimento}</td>
-                <td>$${item.Formacao}</td>
+                <td>${item.ID}</td>
+                <td>${item.Nome}</td>
+                <td>${item.Sobrenome}</td>
+                <td>${item.DtNascimento}</td>
+                <td>${item.Formacao}</td>
             </tr>`)
         })
     }
 }
 
-$(function () {
+$(function() {
     //EXERCUTA AO CARREGAR DA TELA
     dados = JSON.parse(localStorage.getItem("__dados__"))
  
     if (dados) {
         populaTabela
     }
+
+    $("#btnSalvar").click(function() {
+        //EVENTO CLICK DO BOTÃO SALVAR
+
+        let Nome = $("#txtNome").val()
+        let Sobrenome = $("#txtSobrenome").val()
+        let DtNascimento = new Date($("#txtDtNascimento").val()).toLocaleDateString("pt-br", { timeZone: "UTC" })
+        let Formacao = $("#txtFormacao").val()
+
+        let registro = {}
+
+        registro.Nome = Nome
+        registro.Sobrenome = Sobrenome
+        registro.DtNascimento = DtNascimento
+        registro.Formacao = Formacao
+
+        registro.ID = 1
+
+        dados.push(registro)    
+        
+        alert("Registro salvo com sucesso!")
+        $("#modalRegistro").modal("hide")
+
+        //LIMPEZA DOS DADOS
+        $("txtNome").val("")
+        $("txtSobrenome").val("")
+        $("txtDtNascimento").val("")
+        $("txtFormacao").val("")
+
+
+        populaTabela
+    })
 })
